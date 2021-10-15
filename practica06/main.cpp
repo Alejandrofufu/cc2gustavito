066295db837d1bf8d4a7d074f19c410c0278c98f
Alejandro <borsal_ino@hotmail.com>
2021-10-15 15:51:50 -0500

    coctail con puntero a funcion

--- practica06/main.cpp
+++ practica06/main.cpp
@@ -1,12 +1,50 @@
 #include "iostream"
-#include<cstdlib>
-#include<ctime>
-using namespace std;
 
-void numrand(int *arr,int sizeI,int sizeJ){
-    int *pt = arr;
-    for(int i = 0;i<sizeI*sizeJ;i++){
-        *pt = 1+rand()%15;pt++;
+using namespace std;
+void imprimir(int *arr,int size){
+    cout<<"arr: ";
+    for(int *i=arr;i<arr+size;i++)
+        cout<<*i<<", ";
+    cout<<"\n";
+}
+bool ascendente(int a,int b){
+    return a<b;
+}
+bool descendente(int a,int b){
+    return a>b;
+}
+void cockTail(int *Ini,int *Fin,bool (*p)(int,int)){
+    int* inicio = Ini;int*final =Fin;
+    int*o=inicio;o++;int aux = 0;
+    for(int i=0;*final!=*inicio;i++){
+        for(int *j= inicio;j<final;j++){
+            if(p(*j,*o)){o++;}
+            else{
+                aux = *o;
+                *o=*j;
+                *j=aux;
+                o++;
+            }
+        }
+        final--;o=final;o--;
+        for(int *k = final;k>inicio;k--){
+            if(p(*o,*k)){o--;}
+            else{
+                aux=*k;
+                *k = *o;
+                *o=aux;
+                o--;
+            }
+        }
+        inicio++;o=inicio;o++;
     }
 }
-srand(time(NULL));
+int main(){
+    int l[10]={10,20,50,30,22,45,100,2,1,46};
+    cout<<"sin ordenar\n";imprimir(l,10);
+    cockTail(&l[0],&l[9],ascendente);
+    cout<<"\nodernado ascendente\n";imprimir(l,10);
+    cockTail(&l[0],&l[9],descendente);
+    cout<<"\nodernado descendente\n";imprimir(l,10);
+    return 0;
+}
